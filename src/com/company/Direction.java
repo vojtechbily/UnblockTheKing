d36c2fa9284cce25661bf6ede12f80e8ae002845
vojtechbily <bily.vojtech@gmail.com>
2016-03-28 20:14:52 +0200

    Initial upload

---
+++ src/com/company/Direction.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * TODO: Enter a paragraph that summarizes what the class does and why someone might want to utilize it <p> © 2016
+ * NetSuite Inc.
+ *
+ * @author vbily
+ * @since 2016-03-24
+ */
+enum Direction
+{
+	LEFT(),
+	RIGHT(),
+	UP(),
+	DOWN()
+}
